a5ba581bd7295fea54a0eba2404751c42a9b2031
PauLeeR <lindaliel@gmail.com>
2017-06-11 18:34:44 -0400

    koala 2 con tooltip y modal

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// elementos que tienen data-toggle="tooltips" en el documento
+$('[data-toggle="tooltip"]').tooltip();
+
+// elementos a los que le hará un "modal" en el documento
+/*$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})*/      //esta función en js no es necesaria para que funcione el modal en la sección 3
