0b230aac0778b99d04b0af0afeb5299387aafeb7
PauLeeR <lindaliel@gmail.com>
2017-06-11 21:08:42 -0400

    koala 2 con todos los componentes requeridos en el ej evaluado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 // elementos que tienen data-toggle="tooltips" en el documento
 $('[data-toggle="tooltip"]').tooltip();
 
-// elementos a los que le hará un "modal" en el documento
-/*$('#myModal').on('shown.bs.modal', function () {
+/*elementos a los que le hará un "modal" en el documento
+$('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
-})*/      //esta función en js no es necesaria para que funcione el modal en la sección 3
+})  */   //esta función en js no es necesaria para que funcione el modal en la sección 3
+
+/*función para el carousel/slider 
+ $(document).ready(function(){
+        $('.myCarousel').carousel()
+    });     */
+
+/*collapse
+$('.collapse').collapse()*/       
+
